96edd904eaa29d80cbc500b630901f5e9e3e84de
wild1006 <wilder_lp@hotmail.com>
2023-08-18 21:53:57 -0400

    ---Agregado---

---
+++ Elemento.java
@@ -0,0 +1,32 @@
+
+public class Elemento
+{   private String titulo;
+    private int duracion;
+    private boolean loTengo;
+    private String comentario;
+    
+    public Elemento(String elTitulo, int tiempo)
+    {   titulo = elTitulo;
+        duracion = tiempo;
+        loTengo=false;
+        comentario = "";        
+    }
+    
+    public void setComentario(String comentario)
+    {
+        this.comentario =comentario;
+    }
+    public String getComentario(){
+         return comentario;   
+    }
+    public void setLotengo(boolean tengo){
+        loTengo=tengo;
+    }
+    public void print(){
+        System.out.println("Elemento");
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+
+}
